ca20652367e78982cc4917a3a2dd55db870fc4e9
DONGHYEONG <86703459+leedhhhhh@users.noreply.github.com>
2024-08-26 22:46:58 +0900

    Update areThereDuplicates.js

--- 선택문제/areThereDuplicates.js
+++ 선택문제/areThereDuplicates.js
@@ -19,25 +19,33 @@ function areThereDuplicates(...arg) {
 
 // 풀이
 
-function sameFrequency(num1, num2){
-  let strNum1 = num1.toString();
-  let strNum2 = num2.toString();
-  if(strNum1.length !== strNum2.length) return false;
-  
-  let countNum1 = {};
-  let countNum2 = {};
-  
-  for(let i = 0; i < strNum1.length; i++){
-    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+// areThereDuplicates 솔루션 (빈도 수 세기)
+function areThereDuplicates() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
   }
-  
-  for(let j = 0; j < strNum1.length; j++){
-    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+  for(let key in collection){
+    if(collection[key] > 1) return true
   }
-  
-  for(let key in countNum1){
-    if(countNum1[key] !== countNum2[key]) return false;
+  return false;
+}
+// areThereDuplicates 솔루션 (다중 포인터)
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
   }
- 
-  return true;
+  return false
+}
+// areThereDuplicates One Liner 솔루션
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
 }
